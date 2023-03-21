f05385d0d1d64661d359e301adf1b9eef35430ed
Shah Hussain <shahhussain1@me.com>
2023-03-21 18:08:09 +0000

    started backend

---
+++ database/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const routes = require('./routes')
+const mongoose = require('mongoose')
+const mongoString = process.env.MONGO_URI
+
+const app = express()
+
+app.use('/api', routes)
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// app.listen(process.env.PORT, () => {
+//   console.log('listening 4000', process.env.PORT)
+// })
+
+mongoose.connect(mongoString)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('connected & listening 4000', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+// app.listen(4000, () => {
+//   console.log('listening on 4000')
+// })
