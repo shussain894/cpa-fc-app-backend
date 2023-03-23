85b6d2cefb30bfea42fcb75fa77f534c2551a4c8
Shah Hussain <shahhussain1@me.com>
2023-03-23 12:13:21 +0000

    added tokens

--- database/server.js
+++ database/server.js
@@ -1,19 +1,15 @@
 const bp = require('body-parser')
 require('dotenv').config({ path: './database/.env' })
 const express = require('express')
-// const routes = require('./routes')
 const FixtureRoutes = require('./routes/Fixtures')
 const UserRoutes = require('./routes/User')
 const mongoose = require('mongoose')
 const mongoString = process.env.MONGO_URI
 const cors = require('cors')
-
+const JWT = require("jsonwebtoken");
 const app = express()
-app.use(bp.json())
-app.use(bp.urlencoded({extended: true}))
-app.use(cors())
-app.use('/fixtures', FixtureRoutes)
-app.use('/users', UserRoutes)
+const tokensRouter = require("./routes/Token");
+
 
 app.use(express.json())
 
@@ -21,6 +17,25 @@ app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
+
+const tokenChecker = (req, res, next) => {
+  let token;
+  const authHeader = req.get("Authorization");
+
+  if (authHeader) {
+    token = authHeader.slice(7);
+  }
+
+  JWT.verify(token, process.env.JWT_SECRET, (err, payload) => {
+    if (err) {
+      console.log(err);
+      res.status(401).json({ message: "auth error" });
+    } else {
+      req.user_id = payload.user_id;
+      next();
+    }
+  });
+};
 
 mongoose.connect(mongoString)
   .then(() => {
@@ -31,6 +46,13 @@ mongoose.connect(mongoString)
   .catch((error) => {
     console.log(error)
   })
+
+app.use(bp.json())
+app.use(bp.urlencoded({extended: true}))
+app.use(cors())
+app.use('/fixtures', tokenChecker, FixtureRoutes)
+app.use('/users', UserRoutes)
+app.use("/tokens", tokensRouter);
 
 // app.listen(4000, () => {
 //   console.log('listening on 4000')
