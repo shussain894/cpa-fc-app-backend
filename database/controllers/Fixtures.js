687379a90bc62e4f9d7407102bc3619f19bf5ab2
Shah Hussain <shahhussain1@me.com>
2023-04-12 18:02:21 +0100

    fixed fixtures

--- database/controllers/Fixtures.js
+++ database/controllers/Fixtures.js
@@ -2,20 +2,18 @@ const Fixtures = require('../models/Fixtures')
 const TokenGenerator = require('../models/token_generator')
 
 const FixturesController = {
-  Index: (req, res) => {
-    // Fixtures.find(async (err, group) => {
-    //   if (err) {
-    //     console.log('jwt error', err)
-    //     throw err;
-    //   }
-    //   res.status(200).json({ group: group, opponent: opponent, date: date, time: time, venue: venue, confirmed: confirmed, result: result });
-    // });
-    res.status(200).json({message: 'why aint this working'})
-  },
+  Index: async (req, res) => {
+    try { 
+      const response = await Fixtures.find({});
+      console.log('LINE TWELVE', response)
+      res.status(200).json(response)
+    // res.status(200).json({message: 'finally working'})
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+},
 
   Create: async (req, res) => {
-    const { authorization } = req.headers
-    const token = authorization.split(' ')[1]
     try {
       const group = req.body.group;
       const opponent = req.body.opponent;
@@ -28,20 +26,11 @@ const FixturesController = {
       const fixture = new Fixtures(FixtureObject);
   
       await fixture.save();
-      // const token = await TokenGenerator.jsonwebtoken(req.fixture_id);
-      
   
       res.status(201).json({ message: "OK" });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-
-    // fixture.save(async (err) => {
-    //   if (err) {
-    //     throw err;
-    //   }
-    //   res.status(201).json({ message: "OK" });
-    // });
   },
 
   Update: async(req, res) => {
