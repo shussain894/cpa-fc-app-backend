0800cfea73c4ba286a301df8e14cf5576a64b042
Shah Hussain <shahhussain1@me.com>
2023-05-29 16:29:37 +0100

    added child to models

---
+++ database/models/Child.js
@@ -0,0 +1,20 @@
+import { Schema, model } from 'mongoose';
+
+const ChildSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  dob: { type: String, required: true },
+  address: { type: String, required: true },
+  group: { type: String, required: true },
+  school: { type: String, required: true },
+  relationshipToChild: { type: String, required: true },
+  userID: { type: String },
+  nokName: { type: String, required: true },
+  nokNumber: { type: String, required: true },
+  doctorName: { type: String, required: true },
+  surgeryName: { type: String, required: true },
+  surgeryNumber: { type: String, required: true }
+});
+
+const Child = model("Child", ChildSchema);
+
+export default Child;
